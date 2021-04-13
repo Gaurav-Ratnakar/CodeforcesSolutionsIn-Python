8d031c6519c232202ee8d5616e013325c79f6ee8
Gaurav Ratnakar <56241964+Gaurav-Ratnakar@users.noreply.github.com>
2021-04-13 10:29:21 +0530

    Add files via upload

---
+++ F_Swapping Problem.py
@@ -0,0 +1,33 @@
+def PosOfLargestNumber(a):
+    return a.index(max(a))
+
+def PosOfSmallestNumber(a):
+    return a.index(min(a))
+
+def Main(a,b):
+    PositionOfMaxOfa=PosOfLargestNumber(a) 
+    temp=b[PosOfLargestNumber(b)]  
+    b[PosOfLargestNumber(b)]=b[PositionOfMaxOfa]  
+    b[PositionOfMaxOfa]=temp
+    sum=0
+    listoflist=[]
+    listoflist.append(a)
+    listoflist.append(b)
+    for i,j in zip(a,b):
+        sum+=abs(i-j)
+    print(sum)
+
+
+size=int(input())
+a=[]
+b=[]
+for i in range(0,size):
+    x=int(input())
+    a.append(x)
+
+for i in range(0,size):
+    y=int(input())
+    b.append(y)
+
+Main(a,b)
+
